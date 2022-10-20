1f8a1617d614e9a1e1f34689905d39b015e0200c
adipurnama <imadeadipurnamayasa@gmail.com>
2022-10-20 15:43:45 +0800

    layout

---
+++ application/views/home/index.php
@@ -0,0 +1,185 @@
+<nav class="navbar bg-black">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="<?= base_url(); ?>assets/images/tc-chat-new.png">
+        </a>
+        <a href="https://tracklessproject.com" class="bg-trackless me-5">
+            <svg width="57" height="53" viewBox="0 0 57 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                    d="M26.908 1.37204C27.8784 0.750986 29.1214 0.750986 30.0918 1.37204L54.7011 17.122C56.0749 18.0012 56.4758 19.8276 55.5966 21.2013C54.7174 22.575 52.8911 22.9759 51.5174 22.0967L49.1717 20.5955V44.2188C49.1717 48.568 45.646 52.0938 41.2967 52.0938H15.703C11.3538 52.0938 7.828 48.568 7.828 44.2188V20.5955L5.48239 22.0967C4.10868 22.9759 2.28235 22.575 1.40317 21.2013C0.523991 19.8276 0.924893 18.0012 2.29861 17.122L26.908 1.37204ZM19.1483 24.0391C18.3328 24.0391 17.6717 24.7001 17.6717 25.5156C17.6717 26.3311 18.3328 26.9922 19.1483 26.9922H37.8514C38.6669 26.9922 39.328 26.3311 39.328 25.5156C39.328 24.7001 38.6669 24.0391 37.8514 24.0391H19.1483ZM17.6717 31.4219C17.6717 30.6064 18.3328 29.9453 19.1483 29.9453H27.0233C27.8388 29.9453 28.4999 30.6064 28.4999 31.4219C28.4999 32.2374 27.8388 32.8984 27.0233 32.8984H19.1483C18.3328 32.8984 17.6717 32.2374 17.6717 31.4219ZM19.1483 35.8516C18.3328 35.8516 17.6717 36.5126 17.6717 37.3281C17.6717 38.1436 18.3328 38.8047 19.1483 38.8047H30.9608C31.7763 38.8047 32.4374 38.1436 32.4374 37.3281C32.4374 36.5126 31.7763 35.8516 30.9608 35.8516H19.1483Z"
+                    fill="#00DD9C" />
+            </svg>
+        </a>
+    </div>
+</nav>
+<!-- Mashead header-->
+<header class="slider">
+    <div class="container px-5">
+        <div class="row gx-5 pb-5 align-items-center">
+            <div class="col-lg-6">
+                <!-- Mashead text and app badges-->
+                <div class="mb-5 mb-lg-0 text-center text-lg-start">
+                    <span class="header-style">
+                        <h1 class="text-header-trackleschat mb-3 f-poppins">Say it
+                            with
+                            <br>
+                            TracklessChat
+                        </h1>
+                        <img src="<?= base_url(); ?>assets/images/b-text.png" alt="color-green">
+                    </span>
+                    <p class="text-tracklesschat mb-5 f-poppins">It offers the possibility of communicating
+                        with your contacts through message, call & video call completely in secrecy</p>
+                    <h1 class="text-green-gardient-trackless f-poppins mb-5">
+                        Connectable with
+                        any wallet powered by <span>tracklessMoney<img class="question"
+                                src="<?= base_url(); ?>assets/images/question.png" alt="?" data-bs-toggle="modal"
+                                href="#exampleModalToggle" role="button"></span>
+                    </h1>
+
+                    <a href="" class="btn-slchat f-poppins py-3 px-5">Download now</a>
+                </div>
+            </div>
+            <div class="col-lg-6 d-none d-sm-none d-lg-inline">
+                <!-- Masthead device mockup feature-->
+                <div class="masthead-device-mockup text-center">
+                    <img class="img-fluid" src="<?= base_url(); ?>assets/images/phone.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 box-line">
+        <div class="line-header-bottom-left border-start-0 rounded-end"></div>
+        <div class="line-header-bottom-right border-end-0 rounded-start"></div>
+    </div>
+</header>
+<!-- Quote/testimonial aside-->
+
+<aside class="bg-black">
+    <div class="container-fluid px-5 py-md-5">
+        <div class="d-flex justify-content-around flex-wrap">
+            <div class="col-12 col-sm-5 col-md-5 col-lg-3 box-bottom-slider py-5 px-2 px-sm-3 mt-4 mx-3 mx-lg-0">
+                <div class="d-flex align-items-center flex-column mb-3">
+                    <span>What is ?</span>
+                    <p class="mt-2 mt-sm-0"><b class="text-green">TracklessChat</b> is a messaging DApp connectable with
+                        any wallet
+                        powered by <b class="text-green">TracklessMoney</b>
+                        for anonymous micropayments</p>
+                </div>
+            </div>
+            <div class="col-12 col-sm-5 col-md-5 col-lg-3 box-bottom-slider py-5 px-2 px-sm-3 mt-4 mx-3 mx-lg-0">
+                <div class="d-flex align-items-center flex-column mb-3">
+                    <span>System</span>
+                    <p class="mt-2 mt-sm-0"><b class="text-green">TracklessChat</b> is a Decentralized and
+                        Distribuited system, untrackable
+                        & anonymous, that guarantees your privacy </p>
+                </div>
+            </div>
+            <div class="col-12 col-sm-5 col-md-5 col-lg-3 box-bottom-slider py-5 px-2 px-sm-3 mt-4 mx-3 mx-lg-0">
+                <div class="d-flex align-items-center flex-column mb-3">
+                    <span>Additional</span>
+                    <p class="mt-2 mt-sm-0"><b class="text-green">TracklessChat</b> with a powerful translation software
+                        inside, make
+                        easy the communication between
+                        users all around the world</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</aside>
+
+<section class="bg-content-chat py-5">
+    <div class="container p-5">
+        <div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">
+            <div class="col-12 col-lg-6 scr-text">
+                <div>
+                    <h2 class="text-green f-poppins my-3 my-lg-5 m-lg-0">Keep secret your chat<br> <span
+                            class="with-trackless">with <img src="<?= base_url(); ?>assets/images/trackless-nosheal.png"
+                                alt="tracklesschat"></span>
+                    </h2>
+                    <p class="text-white m-0 f-poppins mb-5">ready to get started?</p>
+                    <a href="#" class="btn btn-learn text-green f-poppins py-2 px-4">Download now </a>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="px-5 px-sm-0 d-none d-sm-none d-lg-inline">
+                    <img class="img-fluid" src="<?= base_url() ?>assets/images/phone2.png" alt="image" />
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Footer-->
+<footer class="bg-footer-trackless py-3">
+    <div class="container-fluid px-md-5">
+        <div class="container">
+            <div class="bg-imagebox">
+                <img src="<?= base_url() ?>assets/images/trackless-fit.png" alt="" class="footer-logo-center">
+            </div>
+            <div class="d-flex justify-content-around">
+                <div class="col-4">
+                    <img src="<?= base_url(); ?>assets/images/footer-logo.png" class="footer-logo">
+                    <div class="d-flex flex-wrap icon-sosmed">
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/fb-icon.png" class="p-1">
+                        </div>
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/twitter-icon.png" class="p-1">
+                        </div>
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/ig-icon.png" class="p-1">
+                        </div>
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/c-icon.png" class="p-1">
+                        </div>
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/linkedin-icon.png" class="p-1">
+                        </div>
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/tiktok-icon.png" class="p-1">
+                        </div>
+                        <div class="me-md-2 me-lg-2 me-xl-3 mt-1 mt-sm-3">
+                            <img src="<?= base_url() ?>assets/images/youtube-icon.png" class="p-1">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">&nbsp;</div>
+                <div class="col-4 footer-contact">
+                    <h2>Contact us</h2>
+                    <span>Telegramchanel</span>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex flex-wrap justify-content-end footer-menus">
+            <a href="https://tracklessproject.com" class="">TracklessProject</a> |
+            <a href="https://tracklessmail.com" class="active">TracklessMail</a> |
+            <a href="https://tracklesschat.com" class="">TracklessChat</a> |
+            <a href="https://tracklesscompany.com" class="">TracklessCompany</a> |
+            <a href="https://tracklessbank.com" class="">TracklessBank</a> |
+            <a href="https://tracklesscrypto.com" class="">TracklessCrypto</a> |
+            <a href="https://tracklessmoney.com" class="">TracklessMoney</a>
+        </div>
+    </div>
+</footer>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel"
+    tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content modal-trackless">
+            <div class="modal-header border-0">
+                <button type="button" class="btn btn-close" data-bs-dismiss="modal" aria-label="Close">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-x-lg" viewBox="0 0 16 16">
+                        <path
+                            d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                    </svg>
+                </button>
+            </div>
+            <div class="modal-body text-center">
+                <img src="<?= base_url() ?>assets/images/magnify.png" alt="logo" class="sponsor-modal my-3 mx-3">
+                <img src="<?= base_url() ?>assets/images/piggylogo.png" alt="logo" class="sponsor-modal my-3 mx-3">
+            </div>
+        </div>
+    </div>
+</div>
